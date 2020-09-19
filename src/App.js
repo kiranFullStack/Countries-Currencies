4989778e0dff713ab7e9e61ddf0996bc015adad4
kiranFullStack <kiran.fullstack@outlook.com>
2020-09-19 12:55:52 +0530

    route is changing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import axios from 'axios'
 import './App.scss'
 import { sortBy } from 'lodash'
 import Button from '@material-ui/core/Button'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+} from 'react-router-dom'
 
 function App() {
   const [countriesAllData, setcountriesAllData] = useState([])
@@ -27,8 +35,6 @@ function App() {
       })
   }, [])
 
-  let myFunction = (params) => {}
-
   let sortcountriesDecending = (params) => {
     setcountriesAllData(
       sortBy(countriesAllData, [
@@ -50,30 +56,40 @@ function App() {
   }
 
   return (
-    <div>
-      <Button
-        variant='contained'
-        color='primary'
-        onClick={() => sortcountriesDecending()}
-      >
-        Sort Decending
-      </Button>
-      <Button
-        variant='contained'
-        color='primary'
-        onClick={() => sortcountriesAscending()}
-      >
-        Sort Ascending
-      </Button>
-      {countriesAllData.map((item, index) => (
-        <div key={item.name} onClick={() => console.log(item.name)}>
-          <h1>{item.name}</h1>
-          <h1>{item.capital}</h1>
-          <h1>{item.population}</h1>
-          <img className='flag-icon' src={item.flag} alt='coumntry falg' />
+    <>
+      <Router>
+        <div>
+          <Button
+            variant='contained'
+            color='primary'
+            onClick={() => sortcountriesDecending()}
+          >
+            Sort Decending
+          </Button>
+          <Button
+            variant='contained'
+            color='primary'
+            onClick={() => sortcountriesAscending()}
+          >
+            Sort Ascending
+          </Button>
+          {countriesAllData.map((item, index) => (
+            <Link to={`/${item.name}`}>
+              <div key={item.name} onClick={() => console.log(item.name)}>
+                <h1>{item.name}</h1>
+                <h1>{item.capital}</h1>
+                <h1>{item.population}</h1>
+                <img
+                  className='flag-icon'
+                  src={item.flag}
+                  alt='coumntry falg'
+                />
+              </div>
+            </Link>
+          ))}
         </div>
-      ))}
-    </div>
+      </Router>
+    </>
   )
 }
 
